ccbbd6ee91b0d76d542bdf0bab293a628e58fdc0
Kminzzi <alswl-3016@naver.com>
2022-10-28 22:23:38 +0900

    push

--- app/src/main/java/com/example/YourVoice/ui/CallLog/CallLogFragment.java
+++ app/src/main/java/com/example/YourVoice/ui/CallLog/CallLogFragment.java
@@ -124,7 +124,7 @@ public class CallLogFragment extends Fragment {
                 case R.id.callbtn:
                     if(callText.getText().length()>9) {
                         //mainActivity.ChangeCallActivity((String) callText.getText());
-                        Intent callIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-0000-0000"));
+                        Intent callIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:" + callText.getText()));
                         startActivity(callIntent);
                     }
                     else{
